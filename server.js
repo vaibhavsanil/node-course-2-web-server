51b351793359e4c32eaa058c7510eff119d811c8
Vaibhav Sanil <vaibhav.sanil007@gmail.com>
2018-11-23 19:50:38 +0530

    intial commit

---
+++ server.js
@@ -0,0 +1,62 @@
+const express = require('express');
+const hbs = require('hbs');
+const fs = require('fs');
+
+var app = express();
+
+hbs.registerPartials(__dirname + '/views/partials');
+app.set('view engine','hbs');
+app.use(express.static(__dirname + '/public'));
+
+app.use((req,res, next) => {
+    var now = new Date().toString();
+    //console.log(`${req}`);
+    var log = `${now}: ${req.method} ${req.url}`
+    console.log(log);
+    fs.appendFile('server.log', log + '\n', (err) => {
+        if (err) {
+            console.log('Unable to append to server.log')
+        }
+    });
+    next();
+});
+
+app.use((req,res, next) => {
+    res.render('maintainence.hbs')
+});
+
+hbs.registerHelper('getCurrentYear', () => {
+    return new Date().getFullYear();
+});
+
+
+hbs.registerHelper('screamIt', (text) => {
+    return text.toUpperCase();
+});
+
+app.get('/',(req,res) => {
+        res.render('home.hbs',{
+            pageTitle: 'Welcome Page',
+            welcomeMessage:'Welcome Vaibhav',
+           // currentYear: new Date().getFullYear()
+        });
+    });
+
+
+
+app.get('/about', (req, res) => { 
+    res.render('about.hbs',{
+        pageTitle:'About Page',
+       // currentYear: new Date().getFullYear()
+    });
+});
+
+//Bad Response - Send Back json with error message
+
+app.get('/bad', (req, res) => {
+    res.send({
+        errorMessage: 'Unable to handle request'
+    });
+});
+
+app.listen(3000);
